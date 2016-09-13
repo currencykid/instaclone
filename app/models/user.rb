c6b4283ace52f1d82dfaff424083a48c2e92c54d
ck <thecurrencykid@gmail.com>
2016-09-13 16:24:06 -0700

    first

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  acts_as_voter
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  has_many :active_friendshipss, class_name: "Friendships",
+                                foreign_key: "follower_id",
+                                dependent: :destroy
+end
---
+++ spec/factories/friendships.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :friendship do
+    follower_id 1
+followed_id 1
+  end
+
+end
